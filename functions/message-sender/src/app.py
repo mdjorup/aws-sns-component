c7482247d48750b42f7b9f465232f8b7f952634b
Michael Djorup <michael.djorup@gmail.com>
2022-08-01 08:49:49 -0400

    Made event source data classes for sqs responders

--- functions/message-sender/src/app.py
+++ functions/message-sender/src/app.py
@@ -1,3 +1,10 @@
-def lambda_handler(event, context):
+from aws_lambda_powertools.utilities.data_classes import event_source, SQSEvent
+
+
+@event_source(data_class=SQSEvent)
+def lambda_handler(event: SQSEvent, context):
+
+    for record in event.records:
+        continue
 
     return
