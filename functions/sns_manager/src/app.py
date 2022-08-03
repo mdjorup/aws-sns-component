c3c39485a4f19791e046622ece18ff6abb6661ca
Michael Djorup <michael.djorup@gmail.com>
2022-08-03 13:16:44 -0400

    shared layer added

--- functions/sns_manager/src/app.py
+++ functions/sns_manager/src/app.py
@@ -6,8 +6,6 @@ from aws_lambda_powertools import Logger, Tracer
 from aws_lambda_powertools.event_handler import APIGatewayRestResolver
 from aws_lambda_powertools.logging import correlation_paths
 from aws_lambda_powertools.utilities.typing import LambdaContext
-from aws_lambda_powertools.event_handler.api_gateway import Response
-from aws_lambda_powertools.event_handler import content_types
 from aws_lambda_powertools.event_handler.exceptions import NotFoundError
 from aws_lambda_powertools.middleware_factory import lambda_handler_decorator
 
@@ -53,7 +51,7 @@ def not_found(ex: NotFoundError):
 
 @app.get("/health")
 def health():
-    return build_response(200, {"message": "ok"})
+    return build_response(200, {"message": "OK"})
 
 
 @app.get("/topics")
@@ -129,5 +127,9 @@ def middleware(handler, event, context: LambdaContext):
 @tracer.capture_lambda_handler
 @middleware
 def lambda_handler(event, context: LambdaContext):
+
+    ip_address = event.get("headers", {}).get("X-Forwarded-For", "UNK")
+
+    logger.append_keys(ip_address=ip_address)
 
     return app.resolve(event, context)
