d43534af084501f2e3cd936bd7265062dd1767ea
Michael Djorup <michael.djorup@gmail.com>
2022-08-06 10:29:50 -0400

    Wrote shell scripts for starting locally

--- scripts/update_python.ps1
+++ scripts/update_python.ps1
@@ -1,5 +1,5 @@
 #!/bin/bash
 ./scripts/activate.ps1
 mkdir shared
-pip freeze > shared/requirements.txt
+Copy-Item requirements.txt shared/requirements.txt
 pip install -r shared/requirements.txt -t shared/python --upgrade
